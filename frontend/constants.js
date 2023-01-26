9995751b19ebca13749d4a91cf16e649effee797
Badal Khatri <badal.khatri0924@gmail.com>
2023-01-26 10:44:02 +0530

    Initial Commit

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/badalkhatri0924/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0xA5f02cfA58A2F4EFeD13D9cFeE944d73110104C2";
